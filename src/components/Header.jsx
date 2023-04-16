e51a1fb7a01ee6cffd092bd3bf5d3789141da892
Shoxjahon <shoxjahonniyatov@gmail.com>
2023-04-16 15:57:49 +0500

    f

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from 'react';
 
 export default function Header() {
   return (
-    <div className='bg-black'>
-        <h1>efwe</h1>
+    <div className="border border-dashed flex items-center justify-between w-full p-7">
+      <h1 className="border-b-[#C9A96E] font-bold text-[25px] text-[#171717] tracking-[2.5px] leading-[33px] font-[PT Serif]">
+        FASHION
+      </h1>
+      <nav className="">
+        <ul className="flex items-center gap-[20px]">
+          <li>
+            <a
+              className="hover:text-[#C9A96E] duration-500 text-[#171717] text-[14px] leading-[19px] font-normal font-[PT Serif]"
+              href="/"
+            >
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="/">Recipes</a>
+          </li>
+          <li>
+            <a href="/">Article</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+          <li>
+            <a href="/">Purchase</a>
+          </li>
+        </ul>
+      </nav>
     </div>
-  )
+  );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import bgImg from '../img/bg-img.png';
+
+export default function Main() {
+  return (
+    <div className="">
+      <img className="w-full" src={bgImg} alt="" />
+      <div className="">
+        <h6 className="">Vehicle</h6>
+        <h2>One of Saturn’s largest rings may be newer than anyone</h2>
+        <p>June 6, 2019 By Rickie Baroch 4 comments</p>
+      </div>
+    </div>
+  );
+}
